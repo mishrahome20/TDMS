43008c806bd824abd3e2bf698d44847b24282b65
Don <acharyakeshab20@gmail.com>
2023-10-11 15:46:10 +0545

    Removing Project modals value while clicking the cancel button of modal

--- resources/views/task/project/index.blade.php
+++ resources/views/task/project/index.blade.php
@@ -17,23 +17,36 @@
         </div>
     </div>
     {{--    Modal for Storing Project--}}
-    <x-modal name="project-store-modal">
+    <x-modal name="project-store-modal" id="project_store_modal">
         <form action="{{ route('projects.store') }}" method="post">
             <h2 class="text-center pt-5 text-lg">Create User</h2>
             @csrf
             <div class="bg-white px-4  pb-4 sm:p-6 sm:pb-4">
                 <label class="font-medium text-gray-800">Title</label>
-                <x-text-input type="text" class="w-full outline-none rounded bg-gray-100 p-2 mt-2 mb-3 shadow-lg" id="title"  name="title" placeholder="Enter user Project title"></x-text-input>
+                <x-text-input type="text" class="w-full outline-none rounded bg-gray-100 p-2 mt-2 mb-3 shadow-lg" id="title"  name="title" placeholder="Enter user Project title" value="{{ old('title') }}"></x-text-input>
                 <label class="font-medium text-gray-800">Description</label>
-                <textarea  class="w-full outline-none rounded bg-gray-100 p-2 mt-2 mb-3 shadow" rows="5" id="description" name="description" placeholder="Enter user Description"></textarea>
+                <textarea class="w-full outline-none rounded bg-gray-100 p-2 mt-2 mb-3 shadow" rows="5" id="description" name="description" placeholder="Enter user Description">{{ old('description') }}</textarea>
             </div>
             <div class="bg-gray-200 px-4 py-3 text-right">
-                <x-danger-button x-on:click="$dispatch('close')" id="create_modal_cancel_button">
+                <x-danger-button x-on:click="$dispatch('close')" id="project_create_modal_cancel_button">
                     {{ __('Cancel') }}
                 </x-danger-button>
                 <x-primary-button type="submit">Create</x-primary-button>
             </div>
         </form>
     </x-modal>
+    <script>
+        //Removing Project modal's value while clicking the cancel button of modal
+        document.addEventListener("DOMContentLoaded",function() {
+
+            let ProjectTitle = document.getElementById('title');
+            let ProjectDescription = document.getElementById('description');
+            let ProjectCreateModalCancelButton = document.getElementById('project_create_modal_cancel_button');
+            ProjectCreateModalCancelButton.addEventListener('click',function () {
+                ProjectTitle.value = '';
+                ProjectDescription.value = '';
+            })
+        });
+    </script>
     {{--    Modal for storing Project--}}
 </x-app-layout>
